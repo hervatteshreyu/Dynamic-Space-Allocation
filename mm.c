a63013765556ab20c138126ba8e9a4028f87095a
Shreyas Hervatte Santosh <sjh6186@psu.edu>
2020-02-20 03:52:41 +0000

    Added Seglist framework and helper functions

--- mm.c
+++ mm.c
@@ -1,24 +1,11 @@
+
+
 /*
  * mm.c
  *
  * Name: Shreyas Hervatte Santosh
  *
- *This is a basic implementation of malloc. Throughput and utilization are not being accounted for.
- *This version only aims for correctness of malloc. 
- *Malloc works by increasing the size of the heap everytime malloc is called.
- *It maintains 16 byte alignment of payload addresses by starting off with some padding before the prologue
- *and every malloc returns 16 byte aligned addresses and adds a header and a footer of 8 bytes each
- *
- *Free does essentially nothing but clear the allocated bit of the header of the pointer it receives
- *
- *Realloc simply mallocs and copies the data from the old block.
- *
- *Since malloc and realloc aren't looking for free blocks and since free isn't coelescing free blocks,
- *This model has very bad memory utilization.
- *However due to simple design of malloc, realloc and free, the throughput is higher than the benchmark
- *
- *In further checkpoints, explicit and segregated free list models will be explored to improve utilization and throughput
- *
+ *Note to self - MAKE A COUNTER TO SEE SIZE CLASSES ALONG WITH FREQUENCY OF MALLOC CALLS
  */
 #include <assert.h>
 #include <stdio.h>
@@ -62,23 +49,104 @@
 /* What is the correct alignment? */
 #define ALIGNMENT 16
 
+#define CLASS0 16
+#define CLASS1 32
+#define CLASS2 48 
+#define CLASS3 64
+#define CLASS4 128
+#define CLASS5 512
+#define CLASS6 1024
+#define CLASS7 2048
+#define CLASS8 4096
+#define CLASS9 16384
+#define CLASS10 65536
+#define CLASS11 262144
+#define CLASS12 1048576
+#define CLASS13 4194304
+#define CLASS14 16777216
+#define CLASS15 67108864
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x)
 {
     return ALIGNMENT * ((x+ALIGNMENT-1)/ALIGNMENT);
 }
 
-/*Pointer keeping track of the Head of the free heap*/                                                                                void * head = NULL;
 
+/**********************LINKED LIST*********************/
+typedef struct FreeListNode_s {
+    struct FreeListNode_s * prev;
+    struct FreeListNode_s * next;
+}FreeListNode;
+
+FreeListNode * freelist[16];
+
+void add_free_node(FreeListNode ** head, FreeListNode * node){
+    node->next = *head;
+    node->prev = NULL;
+    (*head)->prev = node;
+}
+
+FreeListNode* remove_free_node(FreeListNode ** head, FreeListNode * node)
+{
+    FreeListNode* removed_node = node;
+    if(*head == node){
+        (*head) = (node->next);
+    }
+    else{
+        node->prev->next = node->next;
+    }
+    if(node->next!=NULL)node->next->prev = node->prev;
+    return removed_node;
+}    
+bool list_at_end(FreeListNode * node)
+{
+    if(node->next == NULL)
+        return true;
+    else
+        return false;
+}
+
+int size_to_class_index(size_t size){
+    if (size == CLASS0) return 0;
+    if (size >CLASS0 && size <=CLASS1) return 1;
+    if (size >CLASS1 && size <=CLASS2) return 2;
+    if (size >CLASS2 && size <=CLASS3) return 3;
+    if (size >CLASS3 && size <=CLASS4) return 4;
+    if (size >CLASS4 && size <=CLASS5) return 5;
+    if (size >CLASS5 && size <=CLASS6) return 6;
+    if (size >CLASS6 && size <=CLASS7) return 7;
+    if (size >CLASS7 && size <=CLASS8) return 8;
+    if (size >CLASS8 && size <=CLASS9) return 9;
+    if (size >CLASS9 && size <=CLASS10) return 10;
+    if (size >CLASS10 && size <=CLASS11) return 11;
+    if (size >CLASS11 && size <=CLASS12) return 12;
+    if (size >CLASS12 && size <=CLASS13) return 13;
+    if (size >CLASS13 && size <=CLASS14) return 14;
+    if (size >CLASS14 && size <=CLASS15) return 15;
+    return -1;
+}
+
+size_t get_size_from_header(void * ptr){
+    return ((size_t) *(((long *)ptr)-1) & ~0xf);
+}
+void set_header_free(void * ptr){
+    long * header = ((long *)ptr-1); 
+    *header = *header & ~0xf;
+}
+size_t size_of_node(FreeListNode * node){
+    return get_size_from_header((void *) node);
+}
 /*
  * Initialize: returns false on error, true on success.
  */
 bool mm_init(void)
 {
+    for(int i=0;i<16;i++){
+        freelist[i]=NULL;
+    }
     /*start is a long pointer - 8bytes long*/
     /*Adding 1 will move the address forward by 8 bytes*/
     long * start = (long*)mem_sbrk((intptr_t)24);
-
     /*Return false for initialization errors*/
     if (start == NULL)return false;
 
@@ -109,7 +177,7 @@ bool mm_init(void)
     start = start + 1;
     
     /*Global head points to address after prologue + header*/
-    head = (void *) start;
+    //    head = (void *) start;
 
     return true;
 }
@@ -120,41 +188,49 @@ bool mm_init(void)
 void* malloc(size_t size)
 {
     
-    if(size>0){
-        
-        /*Extend heap by 16byte aligned size + 16 bytes for header and footer*/
-        /*Store return value for pointer to check for errors with mem_sbrk*/
-        void *temp_head = mem_sbrk((intptr_t)(align(size)+16));
+    if(size<=0)return NULL;
+
+    size = align(size);
+    bool split_flag=false, found_flag=false;
+    int index = size_to_class_index(size);
+    if(freelist[index]!=NULL){
+        FreeListNode * node = freelist[index];
+        while(!list_at_end(node)){
+            size_t blocksize = size_of_node(node);
+            if(size==blocksize){
+                found_flag=true;
+                break;
+            }
+            if(size<blocksize){
+                found_flag=true;
+                split_flag=true;
+                break;
+            }
+            node=node->next;
+        }
+        if(found_flag){
+            return remove_free_node( &freelist[index] , node);
+        }
+        if(split_flag){};
+    }
+
+    
+        char * ret_ptr = mem_heap_hi() + 9;
+        void *temp_head = mem_sbrk((intptr_t)(size+16));
         if (temp_head == (void *)-1)return NULL;
 
-        /*New payload will start from global head - header has already been allocated*/
-        void * ret_ptr = head;
+        char * header=(char *)((long *)ret_ptr - 1), *footer = mem_heap_hi()-7, *epilogue = mem_heap_hi()+1;
 
-        /*Adjust the locations of the payload header, footer and new epilogue*/
-        char * header=head-8, *footer = mem_heap_hi()-7, *epilogue = mem_heap_hi()+1;
-
-        /*Store size in header and set allocated bit*/
-        /*Typecasting to (long *) makes sure all 8 bytes of the  header are being used*/
-        *(long*)header = align(size);
+        *(long*)header = size;
         *(long*)header = *(long*)header | 0x1;
         
-        /*Store size in footer and set allocated bit*/
-        /*Typecasting to (long *) makes sure all 8 bytes of the  header are being used*/
-        *(long*)footer = align(size);
+        *(long*)footer = size;
         *(long*)footer = *(long*)footer | 0x1;
 
-        /*Set empty epilogue and set allocated bit*/
-        /*Typecasting to (long *) makes sure all 8 bytes of the  header are being used*/
         *(long*)epilogue = *(long*)epilogue & 0x0;
         *(long*)epilogue = *(long*)epilogue | 0x1;
 
-        /*Global head points to end of allocated payload + header of next payload*/
-        head = (void *)((long*)epilogue+1);
-
-        return ret_ptr;
-    }
-    /* Return NULL if size<=0 */
-    return NULL;
+        return (void *) ret_ptr;
 }
 
 /*
@@ -162,8 +238,9 @@ void* malloc(size_t size)
  */
 void free(void* ptr)
 {
+    //    size_t size = get_size_from_header(ptr);
     /*Alloc bit of header is cleared*/
-    *((char *)ptr-8) = *((char *)ptr-8) & 0xfffffffffffffff0;
+    set_header_free(ptr);
     return;
 }
 
@@ -187,8 +264,7 @@ void* realloc(void* oldptr, size_t size)
     /*Read size of old block*/
     /*Dereferencing (long*) gives 8 byte value*/
     size_t oldsize = *((long*)oldptr-1);
-    oldsize = (oldsize & 0xfffffffffffffff0);
-
+    oldsize = (oldsize & ~0xf);
     /*Align new size to 16 bytes*/
     size = align(size);
 
